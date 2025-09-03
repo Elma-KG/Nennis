95d3dd08ea86679ab5cb17cc059da229e8cb8d3f
Elma Karen <ekg22@hi.is>
2025-09-03 14:11:16 +0000

    Initial commit - Elma Karen

---
+++ src/main/java/is/hi/hbv501g/nennis/NennisApplication.java
@@ -0,0 +1,13 @@
+package is.hi.hbv501g.nennis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NennisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NennisApplication.class, args);
+    }
+
+}
---
+++ src/test/java/is/hi/hbv501g/nennis/NennisApplicationTests.java
@@ -0,0 +1,13 @@
+package is.hi.hbv501g.nennis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NennisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

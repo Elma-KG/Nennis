d8da0ac9fcc25290c22fa4e7fb164dcadade86e7
KristinFrida <kfs14@hi.is>
2025-09-03 21:54:04 +0000

    Skil á comment frá Kristínu

--- src/main/java/is/hi/hbv501g/nennis/NennisApplication.java
+++ src/main/java/is/hi/hbv501g/nennis/NennisApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class NennisApplication {
 
     //This is team member Sigrún Edda
+    //This is team member Kristín Fríða Sigurborgardóttir
 
     public static void main(String[] args) {
         SpringApplication.run(NennisApplication.class, args);
